e66671e80f38ba74072676730e46098063f8013b
gast1111 <95349208+gast1111@users.noreply.github.com>
2024-02-21 16:42:44 +0100

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -7,8 +7,7 @@ contract ValueTypes{
     bool public _bool1 = !_bool; //取非
     bool public _bool2 = _bool && _bool1; //与
     bool public _bool3 = _bool || _bool1; //或
-    bool public _bool4 = _bool == _bool1; //相等
-    bool public _bool5 = _bool != _bool1; //不相等
+    bool public _bool45345 = _bool || _bool1; //或
 
 
     // 整数
